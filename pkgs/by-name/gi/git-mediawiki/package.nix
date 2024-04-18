2b3cfd130f916a159f3f88a4229d189abade8d43
jopejoe1 <jopejoe1@missing.ninja>
2024-04-18 21:27:58 +0200

    wip

--- pkgs/by-name/gi/git-mediawiki/package.nix
+++ pkgs/by-name/gi/git-mediawiki/package.nix
@@ -4,6 +4,14 @@
 , perl
 }:
 
+let
+  sharness = fetchFromGitHub {
+    owner = "felipec";
+    repo = "sharness";
+    rev = "e94c6bbb04e8772eb9dd587aee3b721c9b01d282";
+    hash = "sha256-hkweQkiR2FU5jaMpNkT83tcyfJ4Ciz2v6tvUrXZJiy4=";
+  };
+in
 stdenv.mkDerivation {
   pname = "git-mediawiki";
   version = "0-unstable-2022-03-10";
@@ -12,9 +20,15 @@ stdenv.mkDerivation {
     owner = "Git-Mediawiki";
     repo = "Git-Mediawiki";
     rev = "73da25d20690cc78e479088a6c0968762411ad41";
-    hash = "";
-    fetchSubmodules = true;
+    hash = "sha256-8o1v4soHYSKCHDF4YN8/+8aq6YsvR4d1n7oHevYtIow=";
   };
+
+  postPatch = ''
+    rm -r t/sharness
+    ln -s ${sharness} t/sharness
+  '';
+
+  env.PREFIX = placeholder "out";
 
   #sourceRoot = "${src.name}/contrib/mw-to-git";
 
