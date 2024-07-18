0572f7e1574f17477a3bc4abafaa000317be49f6
jopejoe1 <johannes@joens.email>
2024-07-18 19:14:14 +0200

    f xeve

--- pkgs/by-name/xe/xeve/package.nix
+++ pkgs/by-name/xe/xeve/package.nix
@@ -4,6 +4,7 @@
   gitUpdater,
   stdenv,
   cmake,
+  fetchpatch2,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -17,9 +18,21 @@ stdenv.mkDerivation (finalAttrs: {
     hash = "sha256-8jXntm/yFme9ZPImdW54jAr11hEsU1K+N5/7RLmITPs=";
   };
 
+  patches = [
+    (fetchpatch2 {
+      name = "fix_compiling_on_clang";
+      url = "https://github.com/mpeg5/xeve/commit/3bcbe24d9b0a58b559998b57d1c6af825d2d59c8.patch";
+      hash = "sha256-g4cPeoT77YbMXZLET9u9WwJgmW2JaXuVUoAqJ0dJwng=";
+    })
+  ];
+
   postPatch = ''
     echo v$version > version.txt
   '';
+
+  cmakeFlags = [
+    (lib.cmakeBool "ARM" stdenv.hostPlatform.isAarch)
+  ];
 
   nativeBuildInputs = [ cmake ];
 
@@ -44,6 +57,5 @@ stdenv.mkDerivation (finalAttrs: {
     mainProgram = "xeve_app";
     maintainers = with lib.maintainers; [ jopejoe1 ];
     platforms = lib.platforms.all;
-    broken = !stdenv.hostPlatform.isx86 || !stdenv.cc.isGNU;
   };
 })
