9b4bfa5880cf5e8b4dea2b13efeb729df1fe6c22
Johannes Jöns <34899572+jopejoe1@users.noreply.github.com>
2024-07-18 16:51:00 +0000

    Update generic.nix

--- pkgs/development/libraries/ffmpeg/generic.nix
+++ pkgs/development/libraries/ffmpeg/generic.nix
@@ -804,7 +804,7 @@ stdenv.mkDerivation (finalAttrs: {
   buildFlags = [ "all" ]
     ++ optional buildQtFaststart "tools/qt-faststart"; # Build qt-faststart executable
 
-  doCheck = stdenv.hostPlatform == stdenv.buildPlatform && !withFullDeps;
+  doCheck = stdenv.hostPlatform == stdenv.buildPlatform;
 
   # Fails with SIGABRT otherwise FIXME: Why?
   checkPhase = let
