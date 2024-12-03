8ebf012ccab65a9b96c0f9cccb12a39771638b42
jopejoe1 <johannes@joens.email>
2024-12-03 21:55:43 +0100

    wiringpi: expose pname and version

--- pkgs/by-name/wi/wiringpi/package.nix
+++ pkgs/by-name/wi/wiringpi/package.nix
@@ -70,8 +70,8 @@ let
 in
 
 symlinkJoin {
-  name = "wiringpi-${version}";
-  inherit passthru;
+  pname = "wiringpi";
+  inherit passthru version;
   paths = [
     passthru.wiringPi
     passthru.devLib
